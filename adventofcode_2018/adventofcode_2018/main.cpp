7594337f4818491bde8c8ad4ec399cfe91d55d5e
Sabrina <sabrina.d.mello@hotmail.com>
2018-12-03 21:30:59 -0500

    Reads from input file: day1_input.txt

--- adventofcode_2018/adventofcode_2018/main.cpp
+++ adventofcode_2018/adventofcode_2018/main.cpp
@@ -1,13 +1,30 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+
+#include "day1.h"
 
 using namespace std;
 
 int main()
 {
-	cout << "Herro World !";
-	cout << "Quick Test";
-	
+	//------------------------------- Start of Program -------------------------------
 
+	cout << "Herro World !";
+	
+	ifstream inFile("day1_input.txt", ios::in);
+
+	if (!inFile)
+	{
+		cout << "Unable to read from file day1_input.txt \n";
+		return -1;
+	}
+
+	char ch;
+	while (inFile.get(ch))
+		cout.put(ch);
+
+	   	 
 
 	//------------------------------- End of Program -------------------------------
 	std::cout << "\n\n\nEND OF PROGRAM" << std::endl;
